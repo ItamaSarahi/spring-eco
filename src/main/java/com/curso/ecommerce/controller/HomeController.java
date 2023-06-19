9fb9e0825c5157c6e7aae8ae5ea9ae40bcd39ce2
ItamaSarahi <19161294@itoaxaca.edu,mx>
2023-06-19 15:18:38 -0600

    cambios

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.repository.ProductoRepository;
 import com.curso.ecommerce.service.ProductoService;
+
+import java.util.Optional;
 
 import org.slf4j.Logger;
 
@@ -27,14 +30,23 @@ public class HomeController {
 	@GetMapping("")
 	public String home(Model model) {
 		model.addAttribute("productos", productoservice.findAll());	
-		return "/usuario/home";
+		return "usuario/home";
 	}
 	
 	
 	//metodo del boton del producto
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id,Model model) {
 		log.info("Id producto enviado como parametro{}",id);
+		Producto producto= new Producto();
+		
+		//Se buscara el producto en la base de datos a traves del producto service
+		Optional<Producto> productoOptional=productoservice.get(id);
+		producto =productoOptional.get();
+	
+		//lo enviamos hacia la vista
+		model.addAttribute("producto",producto);
+		
 		return "usuario/productohome";
 	}
 	
